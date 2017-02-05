28a02358332d0914d5ac33e0676c85ead2a3b504
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-02-05 17:28:39 +0100

    Corrected header, added images.

---
+++ public/javascripts/stylescript.js
@@ -0,0 +1,6 @@
+//
+$('.navbar-item').click(function(){
+	$('.active').removeClass('active');
+    $(this).addClass('active');
+});
+
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '', menu: 'home' });
 });
 
-router.get('/about', function(req, res, next) {
-	res.render('about');
+router.get('/membership', function(req, res, next) {
+	res.render('membership', { title : '| MEMBERSHIP', menu: 'membership'});
+});
+
+router.get('/faq', function(req, res, next) {
+	res.render('faq', { title : '| FAQS', menu: 'faq'});
+});
+
+router.get('/event', function(req, res, next) {
+	res.render('event', { title : '| EVENTS', menu: 'event'});
+});
+
+router.get('/contact', function(req, res, next) {
+	res.render('contact', { title : '| CONTACT', menu: 'contact'});
 });
 
 module.exports = router;

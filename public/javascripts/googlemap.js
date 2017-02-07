372afcc5d212ad7f6237483b2ae946e6fc7ea963
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-02-07 13:22:29 +0100

    Finished contact.pug, added googlemap.js

---
+++ public/javascripts/googlemap.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+   $.getScript('https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false', function()
+	{
+    	var mapOptions = {
+    		center: new google.maps.LatLng(-27.477356,153.0284231),
+    		zoom: 15,
+    		mapTypeId: google.maps.MapTypeId.MAP
+		}
+
+		var map = new google.maps.Map(document.getElementById("contact_map"), mapOptions);
+	});
+};

0ac9ae1986bc8032078dc26485c889dd74130243
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-02-21 15:17:15 +0100

    Did final styling of navbar.

--- public/javascripts/stylescript.js
+++ public/javascripts/stylescript.js
@@ -1,6 +1,17 @@
-//
 $('.navbar-item').click(function(){
 	$('.active').removeClass('active');
     $(this).addClass('active');
 });
 
+$(window).resize(function(e) {
+   setPaddingTop();
+});
+
+$(document).ready(function(e) {
+   setPaddingTop();
+});
+
+function setPaddingTop() {
+	var h = $('#nav').height() + 20;
+   document.body.style.paddingTop = h + 'px';
+}

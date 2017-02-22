b5448e8c9374f6224c283454a9890aa3630395f6
Jørgen Christian Heinsen <c.heinsen@gmail.com>
2017-02-22 08:45:50 +0100

    Added calculation of body padding top when header-image is loaded.

--- public/javascripts/stylescript.js
+++ public/javascripts/stylescript.js
@@ -11,6 +11,10 @@ $(document).ready(function(e) {
    setPaddingTop();
 });
 
+$('.header-image').load(function() {
+	setPaddingTop();
+});
+
 function setPaddingTop() {
 	var h = $('#nav').height() + 20;
    document.body.style.paddingTop = h + 'px';
